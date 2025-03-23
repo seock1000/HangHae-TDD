dd3c0353dde941dc086494a46289d1e2fe111df9
seock1000 <seock1000@github.com>
2025-03-23 17:22:25 +0900

    implement read user point service

---
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,6 @@
+package io.hhplus.tdd.point;
+
+public interface PointService {
+
+    public UserPoint readUserPoint(long userId);
+}
---
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -0,0 +1,19 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointServiceImpl implements PointService {
+
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    @Override
+    public UserPoint readUserPoint(long userId) {
+        return userPointTable.selectById(userId);
+    }
+}
