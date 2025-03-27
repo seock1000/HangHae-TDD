3b8cb15703490a57d8b94e45ede338cc81f38dbf
seock1000 <seock1000@github.com>
2025-03-28 01:31:46 +0900

    동시성 이슈 ReentrantLock 적용

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +17,9 @@ public class PointServiceImpl implements PointService {
 
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
+
+    // lock 객체 생성
+    private final ReentrantLock lock = new ReentrantLock();
 
     @Override
     public UserPoint readUserPoint(long userId) {
@@ -27,35 +31,48 @@ public class PointServiceImpl implements PointService {
         return pointHistoryTable.selectAllByUserId(userId);
     }
 
+    // 작업 중간 에러가 발생하면 이후의 작업이 수행되면 안되므로 try 문으로 묶음
     @Override
     public UserPoint chargeUserPoint(ChargePointDto dto) {
-        UserPointDomain userPointDomain = userPointTable.selectById(dto.userId()).toDomain();
-        userPointDomain.charge(dto.amount());
+        lock.lock();
+        try {
+            UserPointDomain userPointDomain = userPointTable.selectById(dto.userId()).toDomain();
+            userPointDomain.charge(dto.amount());
 
-        UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userPointDomain.getId(), userPointDomain.getPoint());
-        pointHistoryTable.insert(
-                updatedUserPoint.id(),
-                updatedUserPoint.point(),
-                TransactionType.CHARGE,
-                updatedUserPoint.updateMillis()
-        );
+            UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userPointDomain.getId(), userPointDomain.getPoint());
 
-        return updatedUserPoint;
+            pointHistoryTable.insert(
+                    updatedUserPoint.id(),
+                    updatedUserPoint.point(),
+                    TransactionType.CHARGE,
+                    updatedUserPoint.updateMillis()
+            );
+
+            return updatedUserPoint;
+        } finally {
+            lock.unlock();
+        }
     }
 
+    // 작업 중간 에러가 발생하면 이후의 작업이 수행되면 안되므로 try 문으로 묶음
     @Override
     public UserPoint useUserPoint(UsePointDto dto) {
-        UserPointDomain userPointDomain = userPointTable.selectById(dto.userId()).toDomain();
-        userPointDomain.use(dto.amount());
+        lock.lock();
+        try {
+            UserPointDomain userPointDomain = userPointTable.selectById(dto.userId()).toDomain();
+            userPointDomain.use(dto.amount());
 
-        UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userPointDomain.getId(), userPointDomain.getPoint());
-        pointHistoryTable.insert(
-                updatedUserPoint.id(),
-                updatedUserPoint.point(),
-                TransactionType.USE,
-                updatedUserPoint.updateMillis()
-        );
+            UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userPointDomain.getId(), userPointDomain.getPoint());
+            pointHistoryTable.insert(
+                    updatedUserPoint.id(),
+                    updatedUserPoint.point(),
+                    TransactionType.USE,
+                    updatedUserPoint.updateMillis()
+            );
 
-        return updatedUserPoint;
+            return updatedUserPoint;
+        } finally {
+            lock.unlock();
+        }
     }
 }
