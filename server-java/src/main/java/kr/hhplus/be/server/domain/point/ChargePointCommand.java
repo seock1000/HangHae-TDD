c942f2baffea4dcd6eae14bea5b72c572fcdfd75
seock1000 <seock1000@github.com>
2025-04-06 17:15:46 +0900

    feature/Command 생성자 구현

--- server-java/src/main/java/kr/hhplus/be/server/domain/point/ChargePointCommand.java
+++ server-java/src/main/java/kr/hhplus/be/server/domain/point/ChargePointCommand.java
@@ -4,4 +4,12 @@ public record ChargePointCommand(
         long userId,
         int amount
 ) {
+    public ChargePointCommand {
+        if (userId <= 0) {
+            throw new IllegalArgumentException("유효하지 않은 사용자 ID 입니다.");
+        }
+        if (amount <= 0) {
+            throw new IllegalArgumentException("유효하지 않은 충전 금액입니다.");
+        }
+    }
 }
