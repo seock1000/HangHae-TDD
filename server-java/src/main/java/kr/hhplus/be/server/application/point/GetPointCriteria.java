4aab0852cbf0ca92d0ef7289794669435f37ace0
seock1000 <seock1000@github.com>
2025-04-08 01:06:18 +0900

    feature/Point Criteria TC 설계

--- server-java/src/main/java/kr/hhplus/be/server/application/point/GetPointCriteria.java
+++ server-java/src/main/java/kr/hhplus/be/server/application/point/GetPointCriteria.java
@@ -1,5 +1,9 @@
 package kr.hhplus.be.server.application.point;
 
+/**
+ * TC
+ * 사용자 ID가 0보다 작거나 같으면 실패 => IllegalArgumentException, "잘못된 사용자 ID 형식입니다."
+ */
 public record GetPointCriteria(
         long userId
 ) {
