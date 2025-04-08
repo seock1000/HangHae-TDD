e7e59d267a7ef0ebb0a38bf372c80b26a509d2d2
seock1000 <seock1000@github.com>
2025-04-09 00:18:56 +0900

    feature/유저 포인트 조회 command 및 테스트 작성2

--- server-java/src/main/java/kr/hhplus/be/server/domain/point/command/GetUserPointCommand.java
+++ server-java/src/main/java/kr/hhplus/be/server/domain/point/command/GetUserPointCommand.java
@@ -1,2 +1,13 @@
-package kr.hhplus.be.server.domain.point.command;public class GetUserPointCommand {
+package kr.hhplus.be.server.domain.point.command;
+
+import kr.hhplus.be.server.domain.point.error.InvalidUserIdError;
+
+public record GetUserPointCommand(
+        long userId
+) {
+    public GetUserPointCommand {
+        if (userId <= 0) {
+            throw InvalidUserIdError.of("잘못된 사용자 ID 형식입니다.");
+        }
+    }
 }
--- server-java/src/test/java/kr/hhplus/be/server/domain/point/command/GetUserPointCommandTest.java
+++ server-java/src/test/java/kr/hhplus/be/server/domain/point/command/GetUserPointCommandTest.java
@@ -1,4 +1,38 @@
+package kr.hhplus.be.server.domain.point.command;
+
+import kr.hhplus.be.server.domain.point.error.InvalidUserIdError;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GetUserPointCommandTest {
-  
+
+    @Test
+    @DisplayName("GetUserPointCommand 생성 시, 사용자 ID가 0보다 크면 생성에 성공한다.")
+    void testValidUserId() {
+        // given
+        long userId = 1L;
+
+        // when
+        GetUserPointCommand command = new GetUserPointCommand(userId);
+
+        // then
+        assertEquals(userId, command.userId());
+    }
+
+    @Test
+    @DisplayName("GetUserPointCommand 생성 시, 사용자 ID가 0보다 작거나 같으면 InvalidUserIdError가 발생한다.")
+    void testInvalidUserId() {
+        // given
+        long userId = 0L;
+
+        // when
+        Exception exception = assertThrows(InvalidUserIdError.class, () -> new GetUserPointCommand(userId));
+
+        // then
+        assertEquals("잘못된 사용자 ID 형식입니다.", exception.getMessage());
+    }
+
+
 }
