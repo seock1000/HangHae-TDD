31fa0964085ee4bcea14b6962a5ebf6edcb12a74
seock1000 <seock1000@github.com>
2025-04-09 00:04:09 +0900

    feature/포인트 사용 테스트 작성

--- server-java/src/test/java/kr/hhplus/be/server/domain/point/PointServiceTest.java
+++ server-java/src/test/java/kr/hhplus/be/server/domain/point/PointServiceTest.java
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.domain.point;
 
 import kr.hhplus.be.server.domain.point.command.ChargePointCommand;
+import kr.hhplus.be.server.domain.point.command.UsePointCommand;
 import kr.hhplus.be.server.domain.point.error.PointNotExistError;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -68,6 +69,50 @@ class PointServiceTest {
         // when & then
         assertThrows(PointNotExistError.class, () -> {
             pointService.charge(command);
+        });
+    }
+
+    @Test
+    @DisplayName("포인트 사용 시, 포인트를 사용하고 포인트와 사용이력을 저장한다.")
+    void testUsePoint() {
+        // given
+        long givenPointId = 1L;
+        long givenUserId = 1L;
+        int givenBalance = 1_000_000;
+        int useAmount = 1_000_000;
+        Point givenPoint = new Point(givenPointId, givenUserId, givenBalance);
+
+        UsePointCommand command = new UsePointCommand(givenUserId, useAmount);
+
+        Point expectedPoint = new Point(givenPointId, givenUserId, givenBalance - useAmount);
+        PointHistory expectedPointHistory = PointHistory.createUseHistory(expectedPoint, useAmount);
+
+        when(pointRepository.findByUserId(anyLong())).thenReturn(Optional.of(givenPoint));
+        when(pointRepository.save(any())).thenReturn(expectedPoint);
+        when(pointHistoryRepository.save(any())).thenReturn(expectedPointHistory);
+
+        // when
+        pointService.use(command);
+
+        // then
+        verify(pointRepository).findByUserId(givenUserId);
+        verify(pointRepository).save(expectedPoint);
+        verify(pointHistoryRepository).save(expectedPointHistory);
+    }
+
+    @Test
+    @DisplayName("포인트 사용 시, 포인트 정보가 존재하지 않으면 PointNotExistError를 발생시킨다.")
+    void testUsePoint_PointNotExistError() {
+        // given
+        long givenUserId = 1L;
+        int useAmount = 1_000_000;
+        UsePointCommand command = new UsePointCommand(givenUserId, useAmount);
+
+        when(pointRepository.findByUserId(anyLong())).thenReturn(Optional.empty());
+
+        // when & then
+        assertThrows(PointNotExistError.class, () -> {
+            pointService.use(command);
         });
     }
 
