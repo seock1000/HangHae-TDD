83122c3b01ea4416392165f1215c2a57600d60dd
seock1000 <seock1000@github.com>
2025-04-11 03:47:35 +0900

    feature/UserCoupon 도메인 기능 구현

--- server-java/src/main/java/kr/hhplus/be/server/domain/coupon/UserCoupon.java
+++ server-java/src/main/java/kr/hhplus/be/server/domain/coupon/UserCoupon.java
@@ -1,5 +1,7 @@
 package kr.hhplus.be.server.domain.coupon;
 
+import kr.hhplus.be.server.domain.coupon.error.AlreadyUsedCouponError;
+import kr.hhplus.be.server.domain.coupon.error.ExpiredCouponError;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -31,12 +33,22 @@ public class UserCoupon {
      * 이미 사용된 쿠폰이면 사용에 실패한다. => AlreadyUsedCouponError
      * 만료일이 지났으면 사용에 실패한다. => ExpiredCouponError
      */
-    public void use() {}
+    public void use() {
+        if (isUsed) {
+            throw AlreadyUsedCouponError.of("이미 사용된 쿠폰입니다.");
+        }
+        if (coupon.getEndDate().isBefore(LocalDateTime.now())) {
+            throw ExpiredCouponError.of("쿠폰이 만료되었습니다.");
+        }
+        this.isUsed = true;
+    }
 
     /**
      * 쿠폰의 사용 상태를 초기화 한다.
      * TC
      * 성공 케이스 밖에 없음.
      */
-    public void init() {}
+    public void init() {
+        this.isUsed = false;
+    }
 }
